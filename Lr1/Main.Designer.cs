f927ad741689078c69d64c3dc70fc9ff8893f9fc
rewanit <49819859+rewanit@users.noreply.github.com>
2023-09-13 03:37:09 +0300

    Добавьте файлы проекта.

---
+++ Lr1/Main.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Lr1
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            button2 = new Button();
+            button1 = new Button();
+            label4 = new Label();
+            richTextBox2 = new RichTextBox();
+            label3 = new Label();
+            richTextBox1 = new RichTextBox();
+            label2 = new Label();
+            label1 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            textBox1 = new TextBox();
+            tabPage2 = new TabPage();
+            numericUpDown3 = new NumericUpDown();
+            label8 = new Label();
+            numericUpDown2 = new NumericUpDown();
+            label7 = new Label();
+            button3 = new Button();
+            button4 = new Button();
+            label5 = new Label();
+            richTextBox3 = new RichTextBox();
+            label6 = new Label();
+            richTextBox4 = new RichTextBox();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 394);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(button2);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(richTextBox2);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(richTextBox1);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(numericUpDown1);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(792, 361);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Цезарь с ключ. словом";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(140, 172);
+            button2.Name = "button2";
+            button2.Size = new Size(160, 29);
+            button2.TabIndex = 9;
+            button2.Text = "Расшифровать";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(140, 137);
+            button1.Name = "button1";
+            button1.Size = new Size(160, 29);
+            button1.TabIndex = 8;
+            button1.Text = "Зашифровать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(343, 169);
+            label4.Name = "label4";
+            label4.Size = new Size(54, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Вывод";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(343, 192);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(441, 140);
+            richTextBox2.TabIndex = 6;
+            richTextBox2.Text = "";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(343, 3);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Ввод";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(343, 26);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(441, 140);
+            richTextBox1.TabIndex = 4;
+            richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(8, 48);
+            label2.Name = "label2";
+            label2.Size = new Size(124, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Ключевое слово";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(8, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(83, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Смещение";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(163, 12);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(162, 27);
+            numericUpDown1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(163, 45);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(137, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(numericUpDown3);
+            tabPage2.Controls.Add(label8);
+            tabPage2.Controls.Add(numericUpDown2);
+            tabPage2.Controls.Add(label7);
+            tabPage2.Controls.Add(button3);
+            tabPage2.Controls.Add(button4);
+            tabPage2.Controls.Add(label5);
+            tabPage2.Controls.Add(richTextBox3);
+            tabPage2.Controls.Add(label6);
+            tabPage2.Controls.Add(richTextBox4);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(792, 361);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Простые таблицы";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Location = new Point(84, 72);
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(150, 27);
+            numericUpDown3.TabIndex = 19;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(11, 74);
+            label8.Name = "label8";
+            label8.Size = new Size(58, 20);
+            label8.TabIndex = 18;
+            label8.Text = "Строки";
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(84, 39);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(150, 27);
+            numericUpDown2.TabIndex = 17;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(11, 41);
+            label7.Name = "label7";
+            label7.Size = new Size(70, 20);
+            label7.TabIndex = 16;
+            label7.Text = "Столбцы";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(74, 185);
+            button3.Name = "button3";
+            button3.Size = new Size(160, 29);
+            button3.TabIndex = 15;
+            button3.Text = "Расшифровать";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(74, 150);
+            button4.Name = "button4";
+            button4.Size = new Size(160, 29);
+            button4.TabIndex = 14;
+            button4.Text = "Зашифровать";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(277, 182);
+            label5.Name = "label5";
+            label5.Size = new Size(54, 20);
+            label5.TabIndex = 13;
+            label5.Text = "Вывод";
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.Location = new Point(277, 205);
+            richTextBox3.Name = "richTextBox3";
+            richTextBox3.Size = new Size(441, 140);
+            richTextBox3.TabIndex = 12;
+            richTextBox3.Text = "";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(277, 16);
+            label6.Name = "label6";
+            label6.Size = new Size(43, 20);
+            label6.TabIndex = 11;
+            label6.Text = "Ввод";
+            // 
+            // richTextBox4
+            // 
+            richTextBox4.Location = new Point(277, 39);
+            richTextBox4.Name = "richTextBox4";
+            richTextBox4.Size = new Size(441, 140);
+            richTextBox4.TabIndex = 10;
+            richTextBox4.Text = "";
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 394);
+            Controls.Add(tabControl1);
+            Name = "Main";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Button button2;
+        private Button button1;
+        private Label label4;
+        private RichTextBox richTextBox2;
+        private Label label3;
+        private RichTextBox richTextBox1;
+        private Label label2;
+        private Label label1;
+        private NumericUpDown numericUpDown1;
+        private TextBox textBox1;
+        private TabPage tabPage2;
+        private NumericUpDown numericUpDown3;
+        private Label label8;
+        private NumericUpDown numericUpDown2;
+        private Label label7;
+        private Button button3;
+        private Button button4;
+        private Label label5;
+        private RichTextBox richTextBox3;
+        private Label label6;
+        private RichTextBox richTextBox4;
+    }
+}
---
+++ Lr1/Main.cs
@@ -0,0 +1,220 @@
+using System.Diagnostics;
+
+namespace Lr1
+{
+    public partial class Main : Form
+    {
+
+        public String Alphabet { get; set; } = "ABCDEFGHIJKLMNOPQRSTUVWXYZ .,0123456789";
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var keyWord = textBox1.Text;
+            var wordK = Convert.ToInt32(numericUpDown1.Value);
+
+            var alphabet = Alphabet.Select(x => x.ToString()).ToList();
+
+            var text = richTextBox1.Text.Trim();
+
+            var newAlphabet = GenCezarAlphabet(alphabet, keyWord, wordK);
+
+            var codedResult = CodeCezar(alphabet, newAlphabet, text);
+
+            richTextBox2.Text = codedResult;
+
+        }
+
+        private string CodeCezar(List<string> alphabet, List<string> newAlphabet, string text)
+        {
+
+            var newString = "";
+
+            foreach (var character in text)
+            {
+                var index = alphabet.IndexOf(character.ToString());
+                newString += newAlphabet[index];
+            }
+
+            return newString;
+
+
+        }
+
+        private string DeCodeCezar(List<string> alphabet, List<string> newAlphabet, string text)
+        {
+
+            var newString = "";
+
+            foreach (var character in text)
+            {
+                var index = newAlphabet.IndexOf(character.ToString());
+                newString += alphabet[index];
+            }
+
+            return newString;
+
+
+        }
+
+
+        private List<string> GenCezarAlphabet(List<string> alphabet, string keyWord, int wordK)
+        {
+            var newAlphabet = new List<string>();
+            var keyWordAsList = keyWord.Select(x => x.ToString()).ToList();
+            var alphabetWithoutKeyWord = alphabet.Where(x => !keyWord.Contains(x));
+            var partToAddFromFiltredAlphabet = alphabetWithoutKeyWord.Take(0..^wordK);
+            var startPartToAdd = alphabetWithoutKeyWord.Take(^wordK..^0);
+
+            newAlphabet.AddRange(startPartToAdd);
+            newAlphabet.AddRange(keyWordAsList);
+            newAlphabet.AddRange(partToAddFromFiltredAlphabet);
+            return newAlphabet;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var keyWord = textBox1.Text;
+            var wordK = Convert.ToInt32(numericUpDown1.Value);
+
+            var alphabet = Alphabet.Select(x => x.ToString()).ToList();
+
+            var text = richTextBox1.Text.Trim();
+
+            var newAlphabet = GenCezarAlphabet(alphabet, keyWord, wordK);
+
+            var codedResult = DeCodeCezar(alphabet, newAlphabet, text);
+
+            richTextBox2.Text = codedResult;
+        }
+
+
+        private string CodeTable(string text, int columns, int rows)
+        {
+            var tableSize = columns * rows;
+            (columns, rows) = (rows, columns);
+
+            var splitedText = text.Chunk(tableSize);
+
+            var rez = "";
+
+            foreach (var chunk in splitedText)
+            {
+                var tmpArray = new char[rows, columns];
+
+                var tmpString = chunk; 
+
+                if (chunk.Length<tableSize)
+                {
+                    tmpString = string.Join("", tmpString).PadRight(tableSize, ' ').Select(x=>x).ToArray();
+                }
+
+                var chunki = 0;
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int l = 0; l < columns; l++)
+                    {
+                        tmpArray[i, l] = tmpString[chunki++];
+                    }
+                }
+
+
+                for (int i = 0; i < columns; i++)
+                {
+                    for (int l = 0; l < rows; l++)
+                    {
+                        if (tmpArray[l, i] != null)
+                        {
+                            rez += tmpArray[l, i];
+                        }
+                    }
+                }
+
+
+            }
+
+            return rez;
+
+        }
+
+        private string DeCodeTable(string text, int columns, int rows)
+        {
+            var tableSize = columns * rows;
+
+            var splitedText = text.Chunk(tableSize);
+
+            var rez = "";
+
+            foreach (var chunk in splitedText)
+            {
+                var tmpArray = new char[columns, rows];
+
+                var tmpString = chunk;
+
+                if (chunk.Length < tableSize)
+                {
+                    tmpString = string.Join("", tmpString).PadRight(tableSize, ' ').Select(x => x).ToArray();
+                }
+
+                var chunki = 0;
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int l = 0; l < columns; l++)
+                    {
+                        tmpArray[l, i] = tmpString[chunki++];
+                    }
+                }
+
+
+                for (int i = 0; i < columns; i++)
+                {
+                    for (int l = 0; l < rows; l++)
+                    {
+                        if (tmpArray[i, l] != null)
+                        {
+                            rez += tmpArray[i, l];
+                        }
+                    }
+                }
+
+
+            }
+
+            return rez;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            var columns = Convert.ToInt32(numericUpDown2.Value);
+            var rows = Convert.ToInt32(numericUpDown3.Value);
+
+            var text = richTextBox4.Text.Trim();
+
+            var codedResult = CodeTable(text, columns, rows);
+
+            richTextBox3.Text = codedResult;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var columns = Convert.ToInt32(numericUpDown2.Value);
+            var rows = Convert.ToInt32(numericUpDown3.Value);
+
+            var text = richTextBox4.Text.Trim();
+
+            var codedResult = DeCodeTable(text, columns, rows);
+
+            richTextBox3.Text = codedResult;
+        }
+    }
+}
